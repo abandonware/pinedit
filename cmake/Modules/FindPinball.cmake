2988d7e900585f9389a05650cba2e11a5d92b035
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2022-08-05 07:36:24 -0500

    Add /local paths
    If Emlia Pinball was installed from source, it might be in a different place.

--- cmake/Modules/FindPinball.cmake
+++ cmake/Modules/FindPinball.cmake
@@ -11,10 +11,10 @@ pkg_check_modules(PC_PINBALL QUIET pinball-dev)
 find_file(PINBALL_CONFIG pinball-config PATHS /usr/games)
 
 find_path(PINBALL_INCLUDE_DIR Pinball.h
-          HINTS ${PC_PINBALL_INCLUDEDIR} ${PC_PINBALL_INCLUDE_DIRS} /usr/include/pinball
+          HINTS ${PC_PINBALL_INCLUDEDIR} ${PC_PINBALL_INCLUDE_DIRS} /usr/include/pinball /usr/local/include/pinball
           PATH_SUFFIXES pinball )
 
-set(PINBALL_LIBRARY_HINTS ${PC_PINBALL_LIBDIR} ${PC_PINBALL_LIBRARY_DIRS} /usr/lib/pinball /usr/lib/x86_64-linux-gnu/pinball)
+set(PINBALL_LIBRARY_HINTS ${PC_PINBALL_LIBDIR} ${PC_PINBALL_LIBRARY_DIRS} /usr/lib/pinball /usr/local/lib/pinball /usr/lib/x86_64-linux-gnu/pinball)
 find_library(PINBALL_LIBRARY_PIN NAMES libemilia_pin.a
              HINTS ${PINBALL_LIBRARY_HINTS})
 find_library(PINBALL_LIBRARY_ADDON NAMES libemilia_addon.a
